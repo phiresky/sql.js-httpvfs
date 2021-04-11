175656972e1130609059a57cf5a2b889b238ce0f
phiresky <phireskyde+git@gmail.com>
2021-04-11 17:56:51 +0200

    sql.js api for http database

--- sql.js/src/api.js
+++ sql.js/src/api.js
@@ -73,6 +73,7 @@ Module["onRuntimeInitialized"] = function onRuntimeInitialized() {
     var SQLITE_UTF8 = 1;
     // var - cwrap function
     var sqlite3_open = cwrap("sqlite3_open", "number", ["string", "number"]);
+    var sqlite3_open_v2 = cwrap("sqlite3_open_v2", "number", ["string", "number", "number", "string"]);
     var sqlite3_close_v2 = cwrap("sqlite3_close_v2", "number", ["number"]);
     var sqlite3_exec = cwrap(
         "sqlite3_exec",
@@ -323,6 +324,7 @@ Module["onRuntimeInitialized"] = function onRuntimeInitialized() {
         }
         return true;
     };
+    Statement.prototype["bind2"] = Statement.prototype.bind; // https://github.com/GoogleChromeLabs/comlink/blob/4ba8162f6c28fb1bf53b491565ef9a3ae42b72d3/src/comlink.ts#L432
 
     /** Execute the statement, fetching the the next line of result,
     that can be retrieved with {@link Statement.get}.
@@ -790,8 +792,37 @@ Module["onRuntimeInitialized"] = function onRuntimeInitialized() {
         if (data != null) {
             FS.createDataFile("/", this.filename, data, true, true);
         }
-        this.handleError(sqlite3_open(this.filename, apiTemp));
+        const READONLY = 1;
+        const ret = sqlite3_open(this.filename, apiTemp);
         this.db = getValue(apiTemp, "i32");
+        this.handleError(ret);
+        registerExtensionFunctions(this.db);
+        // A list of all prepared statements of the database
+        this.statements = {};
+        // A list of all user function of the database
+        // (created by create_function call)
+        this.functions = {};
+    }
+    function VfsDatabase(filename, vfs) {
+        this.filename = filename;
+        const READONLY = 1;
+        const ret = sqlite3_open_v2(this.filename, apiTemp, 1, vfs);
+        this.db = getValue(apiTemp, "i32");
+        this.handleError(ret);
+        registerExtensionFunctions(this.db);
+        // A list of all prepared statements of the database
+        this.statements = {};
+        // A list of all user function of the database
+        // (created by create_function call)
+        this.functions = {};
+    }
+    function UrlDatabase(url, chunksize = 4096) {
+        this.filename = url.replace("/", "_");
+        const {createLazyFile} = require('../../src/lazyFile');
+        this.lazyFile = createLazyFile(FS, "/", this.filename, url, true, true, chunksize);
+        const ret = sqlite3_open(this.filename, apiTemp, 1, null);
+        this.db = getValue(apiTemp, "i32");
+        this.handleError(ret);
         registerExtensionFunctions(this.db);
         // A list of all prepared statements of the database
         this.statements = {};
@@ -1200,4 +1231,9 @@ Module["onRuntimeInitialized"] = function onRuntimeInitialized() {
 
     // export Database to Module
     Module.Database = Database;
+    Module.VfsDatabase = VfsDatabase;
+    Module.UrlDatabase = UrlDatabase;
+    Module.FS = FS;
+    VfsDatabase.prototype = Object.create(Database.prototype);
+    UrlDatabase.prototype = Object.create(Database.prototype);
 };
